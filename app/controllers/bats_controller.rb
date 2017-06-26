a91b7b15c062a9b6e288824f2701665aa2979152
kat tsukuda <bearheartsbat@gmail.com>
2017-06-26 11:07:19 -0700

    move edit and delete function to from index to show page

--- app/controllers/bats_controller.rb
+++ app/controllers/bats_controller.rb
@@ -1,47 +1,57 @@
 class BatsController < ApplicationController
   def index
     @bats = Bat.all
-    render :index
+  end
+
+  def new
+    @bat = Bat.new
   end
 
   def create
     bat = Bat.new(bat_params)
+
     if bat.save
       redirect_to bat_path(bat)
     end
   end
 
-  def new
-    @bat = Bat.new
-    render :new
-  end
-
   def show
     id = params[:id]
+
     @bat = Bat.find(id)
-    render :show
   end
 
   def edit
     id = params[:id]
+
     @bat = Bat.find(id)
-    render :edit
   end
 
   def update
     id = params[:id]
+
     bat = Bat.find(id)
 
-    updated_attributes = params.require(:bat).permit(:name, :description)
-    bat.update_attributes(updated_attributes)
-    redirect_to edit_bat_path
+    bat.update_attributes(bat_params)
+
+    redirect_to bat_path(bat)
   end
 
   def destroy
     id = params[:id]
+
     @bat = Bat.find(id)
+
     @bat.destroy
+
     redirect_to "/bats"
   end
+
+  private
+
+ def bat_params
+   # whitelist params return whitelisted version
+   params.require(:bat).permit(:name, :description)
+ end
 
 end
