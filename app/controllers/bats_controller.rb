30ce77ba5d20fc1122bf460f737f8c49910106e3
kat tsukuda <bearheartsbat@gmail.com>
2017-06-25 17:16:27 -0700

    add create and new routes and controllers

--- app/controllers/bats_controller.rb
+++ app/controllers/bats_controller.rb
@@ -3,4 +3,18 @@ class BatsController < ApplicationController
     @bats = Bat.all
     render :index
   end
+
+  def new
+    @bat = Bat.new
+    render :new
+  end
+
+  def create
+    bat_params = params.require(:bat).permit(:name, :description)
+    @bat = Bat.new(bat_params)
+    if @bat.save
+      redirect_to "/bat"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'bats#index'
-  resources :bats, only: [:index, :show]
+  resources :bats, only: [:index, :new, :show, :create]
 
   get "/bats", to: "bats#index"
+  get "/bats", to: "bats#new"
   get "/bats/:id", to: "bats#show"
+  post "/creatures", to: "creatures#create"
+
 end
