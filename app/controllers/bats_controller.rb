e724166ab4c41dabf73c4a0849655588d3ef3dd2
kat tsukuda <bearheartsbat@gmail.com>
2017-06-25 21:19:20 -0700

    add delete and edit routes and controllers

--- app/controllers/bats_controller.rb
+++ app/controllers/bats_controller.rb
@@ -4,23 +4,45 @@ class BatsController < ApplicationController
     render :index
   end
 
-  def new
-    @bat = Bat.new
-    render :new
-  end
-
   def create
     bat_params = params.require(:bat).permit(:name, :description)
     @bat = Bat.new(bat_params)
     if @bat.save
-      redirect_to post_path(post.id)
+      redirect_to "/bats/#{bat.id}"
     end
+  end
+
+  def new
+    @bat = Bat.new
+    render :new
   end
 
   def show
     id = params[:id]
     @bat = Bat.find(id)
     render :show
+  end
+
+  def edit
+    id = params[:id]
+    @bat = Bat.find(id)
+    render :edit
+  end
+
+  def update
+    id = params[:id]
+    bat = Bat.find(id)
+
+    updated_attributes = params.require(:bat).permit(:name, :description)
+    bat.update_attributes(updated_attributes)
+    redirect_to edit_bat_path
+  end
+
+  def destroy
+    id = params[:id]
+    @bat = Bat.find(id)
+    @bat.destroy
+    redirect_to "/bats"
   end
 
 end
